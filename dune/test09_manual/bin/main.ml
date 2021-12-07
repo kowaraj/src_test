b866077ad9f147d936af15b538ec7d23dcfbe7bf
kowaraj <kowaraj4stuff@gmail.com>
2021-12-07 11:56:10 +0100

    Up

--- dune/test09_manual/bin/main.ml
+++ dune/test09_manual/bin/main.ml
@@ -1,9 +1,1 @@
-(* let () = print_endline string_of_int Mylib.Mymath.add 1 2 *)
-
-let () = 
-  let result = Mylib.Mymath.add 1 2 in
-  print_endline (string_of_int result)
-
-let () = print_endline (string_of_int (Mylib.Mymath.add 1 2))
-
-let () = Mylib.Mymath.add 1 2 |> Printf.printf "%i\n" 
+let () = Mylib.Mymath.add 1 2 |> print_endline "%i\n" 
