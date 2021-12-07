6c9692d862aebdce6247ba8e7d49b71d6529d3f1
kowaraj <kowaraj4stuff@gmail.com>
2021-12-07 14:11:27 +0100

    Up: missing declaration => unused value 'sub'

--- dune/test09_manual/bin/main.ml
+++ dune/test09_manual/bin/main.ml
@@ -1,1 +1,2 @@
-let () = Mylib.Mymath.add 1 2 |> string_of_int |> print_endline 
+let () = Mylib.Mymath.add 3 2 |> string_of_int |> print_endline 
+let () = Mylib.Mymath.sub 3 2 |> string_of_int |> print_endline 
