d87883844192d472a1f854da37e5770bb1126899
kowaraj <kowaraj4stuff@gmail.com>
2021-12-07 11:55:52 +0100

    Up

--- dune/test09_manual/bin/main.ml
+++ dune/test09_manual/bin/main.ml
@@ -1,1 +1,9 @@
-let () = print_endline "Hello, World!"
+(* let () = print_endline string_of_int Mylib.Mymath.add 1 2 *)
+
+let () = 
+  let result = Mylib.Mymath.add 1 2 in
+  print_endline (string_of_int result)
+
+let () = print_endline (string_of_int (Mylib.Mymath.add 1 2))
+
+let () = Mylib.Mymath.add 1 2 |> Printf.printf "%i\n" 
