f8a1e76427ff312d5c47a6f69814d044219f6a31
kowaraj <kowaraj4stuff@gmail.com>
2021-12-08 15:34:19 +0100

    Using @@ instead of parenthesis

--- dune/test09_manual/bin/main.ml
+++ dune/test09_manual/bin/main.ml
@@ -1,2 +1,5 @@
 let () = Mylib.Mymath.add 3 2 |> string_of_int |> print_endline 
 let () = Mylib.Mymath.sub 3 2 |> string_of_int |> print_endline 
+
+(* no parenthesis!!! *)
+let () = print_endline @@ string_of_int @@ Mylib.Mymath.add 1 2 
